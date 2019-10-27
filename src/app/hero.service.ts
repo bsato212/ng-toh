ea46fac1c9539b12fc805ec849297c0be8eca453
Bruno Borsato <brunoborsato@outlook.com>
2019-10-27 18:19:09 -0300

    Tour of Heroes part 5

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -13,9 +13,20 @@ export class HeroService {
   constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
+    const startTime = Date.now();
+
     return of(HEROES).pipe(
-      delay(1000),
-      tap(heroes => this.messageService.add(`HeroService: fetched [${heroes.length}] heroes`)),
+      delay(Math.random() * 1000),
+      tap(heroes => this.messageService.add(`HeroService: fetched [${heroes.length}] heroes in [${Date.now() - startTime}] ms`)),
+    );
+  }
+
+  getHero(id: number): Observable<Hero> {
+    const startTime = Date.now();
+
+    return of(HEROES.find(item => item.id === id)).pipe(
+      delay(Math.random() * 1000),
+      tap((hero) => this.messageService.add(`HeroService: fetched ID=[${hero.id}] in [${Date.now() - startTime}] ms`)),
     );
   }
 }
